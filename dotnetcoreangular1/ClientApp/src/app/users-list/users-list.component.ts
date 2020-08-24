9b49a689a827124c2ef9ea538d959465c2dbdd35
dkpatel.it <dkpatel.it@gmail.com>
2020-08-24 22:51:21 +0530

    Day 3 changes are done.

--- dotnetcoreangular1/ClientApp/src/app/users-list/users-list.component.ts
+++ dotnetcoreangular1/ClientApp/src/app/users-list/users-list.component.ts
@@ -9,10 +9,55 @@ import { Observable } from 'rxjs';
 })
 export class UsersListComponent implements OnInit {
   public message: string;
+  public gridMessage: string;
   public Users$: Observable<UserModel[]>;
+  public usersArray: UserModel[];
+  public filteredUsers: UserModel[];
+  public pageSize: number;
+  public currentPage: number;
+  public totalPages: number;
+  public pageNumbersList: number[];
 
   constructor(userData: UserDataService) {
+    this.pageSize = 5;
+    this.currentPage = 2;
+    this.totalPages = 0;
     this.Users$ = userData.users$;
+    this.Users$.subscribe(data => this.reloadLocalArray(data));
+  }
+
+  reloadLocalArray(data: UserModel[]) {
+    this.totalPages = Math.ceil(data.length / this.pageSize);
+    this.pageNumbersList = Array(this.totalPages).fill(0).map((x, i) => i + 1);
+    this.usersArray = data;
+
+    this.refreshGridData();
+  }
+
+  setCurrentPage(e) {
+    this.currentPage = e.target.value;
+    this.refreshGridData();
+  }
+
+  setPageSize(e) {
+    this.pageSize = e.target.value;
+    this.currentPage = 1;
+    this.totalPages = Math.ceil(this.usersArray.length / this.pageSize);
+    this.pageNumbersList = Array(this.totalPages).fill(0).map((x, i) => i + 1);
+    this.refreshGridData();
+  }
+
+  refreshGridData() {
+    var startIndex = ((this.currentPage - 1) * this.pageSize);
+    var endNumber = this.currentPage * this.pageSize;
+
+    this.filteredUsers = this.usersArray.slice(startIndex, endNumber);
+
+    if (endNumber > this.usersArray.length) {
+      endNumber = this.usersArray.length;
+    }
+
+    this.gridMessage = "Showing " + (startIndex + 1) + " to " + endNumber + " of " + this.usersArray.length + " records.";
   }
 
   ngOnInit() {
